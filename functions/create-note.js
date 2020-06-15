30931ef2df7129d79ba6446ab3f9917fc851bf26
Jesper Strange Klitgaard Christiansen <jesperkc@gmail.com>
2020-06-15 13:46:09 +0200

    Functions

--- functions/create-note.js
+++ functions/create-note.js
@@ -1,27 +1,32 @@
-const sendQuery = require("./helpers/send-query");
+const sendQuery = require('./helpers/send-query');
 
 const CREATE_NOTE = `
-  mutation($text: String!){
-    createNote(data: {text: $text}){
+  mutation($text: String!, $userID: String!){
+    createNote(data: {text: $text, userID: $userID}){
       _id
       text
+      userID
     }
   }
 `;
 
-exports.handler = async event => {
-  const { text } = JSON.parse(event.body);
-  const { data, errors } = await sendQuery(CREATE_NOTE, { text });
+exports.handler = async (event, context, callback) => {
+	const { identity, user } = context.clientContext;
+	const { text } = JSON.parse(event.body);
+	console.log(context);
+	if (!identity) throw new Error('Not Authorized');
+	const userID = (identity && identity.user && identity.user.id) || 'none';
+	const { data, errors } = await sendQuery(CREATE_NOTE, { text, userID });
 
-  if (errors) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify(errors)
-    };
-  }
+	if (errors) {
+		return {
+			statusCode: 500,
+			body: JSON.stringify(errors),
+		};
+	}
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ newNote: data.createNote })
-  };
+	return {
+		statusCode: 200,
+		body: JSON.stringify({ newNote: data.createNote }),
+	};
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,69 +1,68 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Note from '../components/note';
 import Form from '../components/form';
 import './index.css';
-import IdentityModal, { useIdentityContext } from "react-netlify-identity-widget"
-import "react-netlify-identity-widget/styles.css"
+import IdentityModal, { useIdentityContext } from 'react-netlify-identity-widget';
+import 'react-netlify-identity-widget/styles.css';
 
 export default () => {
-  const [status, setStatus] = useState("loading");
-  const [notes, setNotes] = useState(null);
-  useEffect(() => {
-    let canceled = false;
-    if (status != "loading") return;
-    axios("/api/get-all-notes").then(result => {
-      if (canceled === true) return;
-      
-      if (result.status != 200) {
-        console.error("Error loading notes");
-        console.error(result);
-        return;
-      }
-      setNotes(result.data.notes);
-      setStatus("loaded");
-    });
+	const [status, setStatus] = useState('loading');
+	const [notes, setNotes] = useState(null);
+	useEffect(() => {
+		let canceled = false;
+		if (status != 'loading') return;
+		axios('/api/get-all-notes').then(result => {
+			if (canceled === true) return;
 
-    return () => {
-      canceled = true;
-    };
-  }, [status]);
+			if (result.status != 200) {
+				console.error('Error loading notes');
+				console.error(result);
+				return;
+			}
+			setNotes(result.data.notes);
+			setStatus('loaded');
+		});
 
-  const reloadNotes = () => setStatus('loading');
-  
-  const identity = useIdentityContext()
-  const [dialog, setDialog] = React.useState(false)
-  const name =
-    (identity && identity.user && identity.user.user_metadata && identity.user.user_metadata.full_name) || "NoName"
-  const isLoggedIn = identity && identity.isLoggedIn
+		return () => {
+			canceled = true;
+		};
+	}, [status]);
 
-  return (
-      <main>
-        <h1>The Writing Pad</h1>
-        {identity && identity.isLoggedIn ? (
-          <>
-            <button className="login-btn" onClick={() => setDialog(true)}>
-              {isLoggedIn ? `Hello ${name}, Log out here!` : "LOG IN"}
-            </button>
-            <Form reloadNotes={reloadNotes}/>
-              {notes ? (
-                <ul>
-                  {notes.map(note => (
-                    <li key={note._id}>
-                      <Note note={note} reloadNotes={reloadNotes}/>
-                    </li>
-                  ))}
-                </ul>
-              ) : (
-                <p>Loading notes...</p>
-              )}
-          </>
-        ) : (
-          <button className="login-btn" onClick={() => setDialog(true)}>
-            {isLoggedIn ? `Hello ${name}, Log out here!` : "LOG IN"}
-          </button>
-        )}
-        <IdentityModal showDialog={dialog} onCloseDialog={() => setDialog(false)} />
-      </main>
-  );
+	const reloadNotes = () => setStatus('loading');
+
+	const identity = useIdentityContext();
+	const [dialog, setDialog] = React.useState(false);
+	const name = (identity && identity.user && identity.user.user_metadata && identity.user.user_metadata.full_name) || 'NoName';
+	const isLoggedIn = identity && identity.isLoggedIn;
+	console.log(identity.user.id);
+	return (
+		<main>
+			<h1>The Writing Pad</h1>
+			{identity && identity.isLoggedIn ? (
+				<>
+					<button className='login-btn' onClick={() => setDialog(true)}>
+						{isLoggedIn ? `Hello ${name}, Log out here!` : 'LOG IN'}
+					</button>
+					<Form reloadNotes={reloadNotes} />
+					{notes ? (
+						<ul>
+							{notes.map(note => (
+								<li key={note._id}>
+									<Note note={note} reloadNotes={reloadNotes} />
+								</li>
+							))}
+						</ul>
+					) : (
+						<p>Loading notes...</p>
+					)}
+				</>
+			) : (
+				<button className='login-btn' onClick={() => setDialog(true)}>
+					{isLoggedIn ? `Hello ${name}, Log out here!` : 'LOG IN'}
+				</button>
+			)}
+			<IdentityModal showDialog={dialog} onCloseDialog={() => setDialog(false)} />
+		</main>
+	);
 };
